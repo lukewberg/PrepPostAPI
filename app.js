388f1b7e5d6c1c9080e4d5ebbd58210e09826175
Unknown <lukewberg@gmail.com>
2018-04-07 13:41:11 -0700

    Modified Dependencies
    Modified more dependencies

--- app.js
+++ app.js
@@ -4,11 +4,27 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
+var colors = require('colors');
+var passport = require('passport');
+var session = require('express-session')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.database);
+
+mongoose.connection.once('open', function(){
+  console.log('Conenction to the database successful!'.cyan);
+});
 
 var app = express();
+
+// required for passport
+app.use(session({ secret: 'goforthandsettheworldonfire' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,8 +32,16 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+require('./app/routes.js')(app, passport);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// Initialize passport
+app.use(passport.initialize());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,7 +56,6 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
