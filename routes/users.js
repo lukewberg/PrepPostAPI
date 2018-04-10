8cd09360db889f8f21bcc34da7c9592602b3ae9b
Unknown <lukewberg@gmail.com>
2018-04-09 21:37:59 -0700

    Added  jwt capabilities
    The login function can auhenticate a user and will return a Json Web Token, which will later be used to lockdown routes for access only by authenticated users.

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,56 @@ var bcrypt = require('bcrypt');
 var userModel = require('../models/userModel');
 var mongoose = require('mongoose');
 var color = require('colors')
+var jwt = require('jsonwebtoken')
 
 var userFunctions = {
 
-  // This function adds a userafter it has validated the email address and hashed the password
+  // This function will handle user login and will issue a token when successfully authenticated
+  login: function (req, res) {
+    userModel.find({
+        email: req.body.email
+      })
+      .then(function (user) {
+        if (user.length < 1) {
+          res.status(401).json({
+            MESSAGE: 'Authentication failed!'
+          })
+        } else {
+          bcrypt.compare(req.body.password, user[0].password, function (error, result) {
+            if (result) {
+              jwt.sign({
+                email: user[0].email,
+                _id: user[0]._id
+              }, '**goforthandsettheworldonfire**', {
+                expiresIn: '1h'
+              }, function (error, token) {
+                if (token) {
+                  res.status(200).json({
+                    MESSAGE: 'Authentication successful!',
+                    TOKEN: token
+                  })
+                } else {
+                  res.status(500).json({
+                    MESSAGE: 'Internal error!'
+                  })
+                }
+              })
+            } else {
+              res.status(401).json({
+                MESSAGE: 'Authentication failed!'
+              })
+            }
+          })
+        }
+      })
+      .catch(function (error) {
+        res.status(500).json({
+          ERROR: error.message
+        })
+      })
+  },
+
+  // This function adds a user after it has validated the email address and hashed the password
   signup: function (req, res) {
     bcrypt.hash(req.body.password, 10, function (error, result) {
       console.log(result)
@@ -57,6 +103,10 @@ var userFunctions = {
 router.route('/signup')
   .post(userFunctions.signup)
 
+router.route('/login')
+  .post(userFunctions.login)
+
 router.route('/delete/:userID')
   .get(userFunctions.delete)
-module.exports = router;
+
+  module.exports = router;
